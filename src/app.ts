097cfe54a64467903fe068c5c9e88148801cda2d
EdmilsonFerreiraF <Edmilsonf.o3@gmail.com>
2021-10-28 13:12:14 -0300

    🚀 :rocket: [Add] Authenticate user
    Sign in by passing in request body Github code, so provide user data

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,21 @@
 import express from 'express'
 
+import { router } from './routes'
+
 const app = express()
+
+app.use(express.json())
+app.use(router)
+
+app.get("/github", (request, response) => {
+    response.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`)
+})
+
+app.get("/signin/callback", (request, response) => {
+    const { code } = request.query
+
+    return response.json(code)
+})
 
 const PORT = 4000
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
